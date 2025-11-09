fae94eb5e7dd7c70a457369454cfccb1b1ec5383
sn-be <170857968+sn-be@users.noreply.github.com>
2025-11-10 02:03:16 +0500

    chore: remove DATABASE_URL from runtime environment configuration

--- src/env.js
+++ src/env.js
@@ -32,7 +32,6 @@ export const env = createEnv({
 	 * middlewares) or client-side so we need to destruct manually.
 	 */
 	runtimeEnv: {
-		DATABASE_URL: process.env.DATABASE_URL,
 		TURSO_DATABASE_URL: process.env.TURSO_DATABASE_URL,
 		TURSO_DATABASE_AUTH_TOKEN: process.env.TURSO_DATABASE_AUTH_TOKEN,
 		NODE_ENV: process.env.NODE_ENV,
